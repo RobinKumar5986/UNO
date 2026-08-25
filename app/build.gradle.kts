f8a3831b43145e046df272dc45f16a5b6fb765d0
Robin Kumar <rkrobin6550@gmail.com>
2026-08-26 00:36:53 +0530

    Optimized the system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.kgjr.uno"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.kgjr.uno"
@@ -29,9 +25,12 @@ android {
             )
         }
     }
+    aaptOptions {
+        noCompress("tflite")
+    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
@@ -43,4 +42,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // TensorFlow Lite dependencies
+    implementation("org.tensorflow:tensorflow-lite:2.11.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.11.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu-api:2.11.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.2")
+
+    // Camera
+    implementation("androidx.camera:camera-core:1.2.3")
+    implementation("androidx.camera:camera-camera2:1.2.3")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // coroutines for parallel computation
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
